ee73dac140fd461128a6203c9bc5eb776842f525
shigh <highscot@gmail.com>
2013-09-30 22:14:25 -0400

    Added IDCSolver class

--- pyidc/solvers.py
+++ pyidc/solvers.py
@@ -47,3 +47,34 @@ def idc_order(f, ps_base, method, order, actual, pows, ret_errs=False, dtype=np.
         return (-slope, errs)
     else:
         return -slope    
+
+class IDCSolver(object):
+
+    def __init__(self, f, ps, method, order, dtype=np.double, n_corr=None):
+        self.f      = f
+        self.ps     = ps
+        self.method = method
+        self.order  = order
+        self.dtype  = dtype
+        self.n_corr = n_corr
+
+    def solve(self):
+        """ A single time step of IDC
+        """
+
+        t_vals = np.linspace(self.ps.rng[0], self.ps.rng[1], self.ps.n_points)
+        y_vals = np.zeros_like(t_vals).astype(self.dtype)
+        y_vals[0] = self.ps.y0
+
+        n_corr = order-1 if self.n_corr==None else self.n_corr
+
+        for i in range(self.ps.n_points-1):
+            s = ProblemSetup(y_vals[i], (t_vals[i], t_vals[i+1]), self.order)
+            m = method(self.f, self.s)
+            m.predict()
+            for j in range(n_corr): m.correct()
+
+            y_vals[i+1] = m.eta[-1]
+
+        return y_vals    
+        
